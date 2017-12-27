27386b930db03f6a87acebdf4d41f05ee3f39a5b
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2017-12-26 21:47:46 -0800

    Minimal play feature at text position (then not empty).

--- test.py
+++ test.py
@@ -3,8 +3,12 @@
 import unittest
 
 class Grid:
+    def __init__(self) -> None:
+        self.played = False
     def is_empty(self) -> bool:
-        return True
+        return not self.played
+    def play(self, position: str) -> None:
+        self.played = True
 
 class TicTacToeTest(unittest.TestCase):
     def test_havegrid(self):
@@ -13,6 +17,10 @@ class TicTacToeTest(unittest.TestCase):
     def test_startgrid_is_empty(self):
         grid = Grid()
         assert(grid.is_empty())
+    def test_play_center_after_empty(self):
+        grid = Grid()
+        grid.play('center')
+        assert(not grid.is_empty())
 
 if __name__ == '__main__':
     unittest.main()
