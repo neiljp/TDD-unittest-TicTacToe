04f4bd8483b2e4c9cd8e4bac1e300904413a0ebe
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2017-12-27 22:41:29 -0800

    Extract self.grid into setUp.

--- test.py
+++ test.py
@@ -13,20 +13,18 @@ class Grid:
         return clear_position
 
 class TicTacToeTest(unittest.TestCase):
+    def setUp(self):
+        self.grid = Grid()
     def test_havegrid(self):
-        grid = Grid()
-        assert(grid is not None)
+        assert(self.grid is not None)
     def test_startgrid_is_empty(self):
-        grid = Grid()
-        assert(grid.is_empty())
+        assert(self.grid.is_empty())
     def test_not_empty_after_play_center(self):
-        grid = Grid()
-        assert(grid.play('center'))
-        assert(not grid.is_empty())
+        assert(self.grid.play('center'))
+        assert(not self.grid.is_empty())
     def test_play_center_twice_fails(self):
-        grid = Grid()
-        assert(grid.play('center'))
-        assert(not grid.play('center'))
+        assert(self.grid.play('center'))
+        assert(not self.grid.play('center'))
 
 if __name__ == '__main__':
     unittest.main()
