0019c1df43b9cfb35f46b6c7536a58710a35e791
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2017-12-28 17:42:08 -0800

    Migrate helper functions into test base class & consistently prefix.

--- test.py
+++ test.py
@@ -95,6 +95,24 @@ class TicTacToeTest(unittest.TestCase):
         self.grid.play('center')
         self.assertEqual(self.grid.get_winning_player(), None)
 
+    def _make_plays(self, x_plays, o_plays, grid=None):
+        if grid is None:
+            grid = self.grid
+        plays = x_plays + o_plays
+        plays[::2] = x_plays
+        plays[1::2] = o_plays
+        for play in plays:
+            grid.play(play)
+    def _get_grids_for_multiple_encoded_plays(self, x_plays, o_plays):
+        grids = []
+        for game_x, game_o in zip(x_plays, o_plays):
+            grid = self.make_grid()
+            game_x = [Grid.textual_positions[i] for i in game_x]
+            game_o = [Grid.textual_positions[j] for j in game_o]
+            self._make_plays(game_x, game_o, grid)
+            grids.append((grid, game_x, game_o))
+        return grids
+
 class TicTacToeTest_XO(TicTacToeTest):
     def test_alternating_play_marks(self):
         self.assertEqual(self.grid.play('center'), 'X')
@@ -126,14 +144,6 @@ class TicTacToeTest_XO(TicTacToeTest):
         for play in plays:
             self.grid.play(play)
         self.assertEqual(self.grid.get_winning_player(), 'O')
-    def _make_plays(self, x_plays, o_plays, grid=None):
-        if grid is None:
-            grid = self.grid
-        plays = x_plays + o_plays
-        plays[::2] = x_plays
-        plays[1::2] = o_plays
-        for play in plays:
-            grid.play(play)
     def test_O_player_should_win_on_top(self):
         X_plays = ['bottom_left', 'bottom_middle', 'center']
         O_plays = ['top_left', 'top_middle', 'top_right']
@@ -154,44 +164,35 @@ class TicTacToeTest_XO(TicTacToeTest):
         O_plays = ['top_middle', 'center', 'bottom_middle']
         self._make_plays(X_plays, O_plays)
         self.assertEqual(self.grid.get_winning_player(), 'O')
-    def get_grids_for_multiple_encoded_plays(self, x_plays, o_plays):
-        grids = []
-        for game_x, game_o in zip(x_plays, o_plays):
-            grid = self.make_grid()
-            game_x = [Grid.textual_positions[i] for i in game_x]
-            game_o = [Grid.textual_positions[j] for j in game_o]
-            self._make_plays(game_x, game_o, grid)
-            grids.append((grid, game_x, game_o))
-        return grids
     def test_X_player_should_win_horizontally_x3(self):
         X_plays = [[0,1,2], [3,4,5], [6,7,8]]
         O_plays = [[3,4], [6,7], [0,1]]  # Abitrary valid other moves
-        for grid, x, o in self.get_grids_for_multiple_encoded_plays(X_plays, O_plays):
+        for grid, x, o in self._get_grids_for_multiple_encoded_plays(X_plays, O_plays):
             self.assertEqual(grid.get_winning_player(), 'X', (x, o))
     def test_X_player_should_win_vertically_x3(self):
         X_plays = [[0,3,6], [1,4,7], [2,5,8]]
         O_plays = [[1,2], [2,3], [3,4]]  # Abitrary valid other moves
-        for grid, x, o in self.get_grids_for_multiple_encoded_plays(X_plays, O_plays):
+        for grid, x, o in self._get_grids_for_multiple_encoded_plays(X_plays, O_plays):
             self.assertEqual(grid.get_winning_player(), 'X', (x, o))
     def test_X_player_should_win_diagonally_x2(self):
         X_plays = [[0,4,8], [2,4,6]]
         O_plays = [[1,2], [3,5]]  # Abitrary valid other moves
-        for grid, x, o in self.get_grids_for_multiple_encoded_plays(X_plays, O_plays):
+        for grid, x, o in self._get_grids_for_multiple_encoded_plays(X_plays, O_plays):
             self.assertEqual(grid.get_winning_player(), 'X', (x, o))
     def test_O_player_should_win_horizontally_x3(self):
         X_plays = [[0,1,6], [3,4,1], [6,7,3]]  # Abitrary valid other moves
         O_plays = [[3,4,5], [6,7,8], [0,1,2]]
-        for grid, x, o in self.get_grids_for_multiple_encoded_plays(X_plays, O_plays):
+        for grid, x, o in self._get_grids_for_multiple_encoded_plays(X_plays, O_plays):
             self.assertEqual(grid.get_winning_player(), 'O', (x, o))
     def test_O_player_should_win_vertically_x3(self):
         X_plays = [[0,3,5], [1,4,5], [1,4,6]]  # Abitrary valid other moves
         O_plays = [[1,4,7], [0,3,6], [2,5,8]]
-        for grid, x, o in self.get_grids_for_multiple_encoded_plays(X_plays, O_plays):
+        for grid, x, o in self._get_grids_for_multiple_encoded_plays(X_plays, O_plays):
             self.assertEqual(grid.get_winning_player(), 'O', (x, o))
     def test_O_player_should_win_diagonally_x2(self):
         X_plays = [[1,3,7], [1,0,3]]  # Abitrary valid other moves
         O_plays = [[0,4,8], [2,4,6]]
-        for grid, x, o in self.get_grids_for_multiple_encoded_plays(X_plays, O_plays):
+        for grid, x, o in self._get_grids_for_multiple_encoded_plays(X_plays, O_plays):
             self.assertEqual(grid.get_winning_player(), 'O', (x, o))
 
 class TicTacToeTest_OX(TicTacToeTest):
